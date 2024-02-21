2f5fc055669b3fbc0972a4ca6c48511f1be0f315
BiagioScaglia308 <97605791+BiagioKawasaki308@users.noreply.github.com>
2024-02-21 19:04:50 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + "! How are you?");
+            Console.WriteLine("Enter a number:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter another number:");
+            int b = int.Parse(Console.ReadLine());
+            int result = a + b;
+            Console.WriteLine("The result is " + result + "!");
+        }
+    }
+}
